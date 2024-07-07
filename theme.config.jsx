200fa2b9c0d15d228ea1827e48b267aedcbb971c
Thomas Clowes <thomas@doublenegative.com>
2024-07-07 15:24:13 +0200

    Initial commit

---
+++ theme.config.jsx
@@ -0,0 +1,12 @@
+export default {
+  logo: (
+    <>
+      <img src="images/logo-black.png" alt="Unruggable logo" style={{"width":"30px", marginRight: "5px"}} />
+      <span>Unruggable Gateways</span>
+    </>
+  ),
+  project: {
+    link: 'https://github.com/shuding/nextra'
+  }
+  // ... other theme options
+}
