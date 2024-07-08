b174d9dd26ddc13cc5a3ecd511b4b5eba5b272b6
Thomas Clowes <thomas@doublenegative.com>
2024-07-08 08:23:28 +0200

    - Added motivations

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,7 +2,7 @@ export default {
   logo: (
     <>
       <img src="images/logo-black.png" alt="Unruggable logo" style={{"width":"30px", marginRight: "5px"}} />
-      <span>Unruggable Gateways</span>
+      <span>Gateway Documentation</span>
     </>
   ),
   project: {
