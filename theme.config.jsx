026f7f5fd782906ee13a83e5236febe3a26728f7
Thomas Clowes <thomas@doublenegative.com>
2024-09-02 16:33:04 +0100

    - Completed release documentation

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,8 +1,13 @@
 export default {
   logo: (
     <>
-      <img src="images/logo-black.png" alt="Unruggable logo" style={{"width":"30px", marginRight: "5px"}} />
+      <img src="/images/logo-black.png" alt="Unruggable logo" style={{"width":"30px", marginRight: "5px"}} />
       <span>Gateway Documentation</span>
+    </>
+  ),
+  head: (
+    <>
+      <link rel="icon" type="image/x-icon" href="/images/logo-black.png"/>
     </>
   ),
   project: {
@@ -12,6 +17,12 @@ export default {
     return {
       titleTemplate: '%s – Gateway Documentation',
     }
-  }
+  },
+  docsRepositoryBase: 'https://github.com/unruggable-labs/evmgateway-v2-docs/',
+  sidebar: {
+    defaultMenuCollapsed: false,
+    toggleButton: true,
+    forcedNavOrder: true, // This ensures that files not listed in _meta.json are not displayed.
+  },
   // ... other theme options
 }
